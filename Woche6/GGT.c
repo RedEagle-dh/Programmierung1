cff2e904c4f75c7b0c07e89349ef44ce01a3e18c
dh90735 <david.hermann@edag.com>
2022-12-02 16:37:32 +0100

    Bugfixes in GGT und KGV

--- Woche6/GGT.c
+++ Woche6/GGT.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 int ggT(int a, int b) {
-    if (a == 0)
-        return b;
-    while (b != 0) {
-        if (a > b)
-            a = a - b;
-        else
-            b = b - a;
-    }
+    if (a == 0) return b;
 
+    while (b != 0) {
+        if (a > b) {
+            a = a - b;
+        } else {
+            b = b - a;
+        }
+    }
     return a;
 }
 
 int main() {
     int a, b;
-    printf("Geben Sie zuerst die groessere, dann die kleinere Zahlen ein:\n");
+    printf("Geben Sie 2 Zahlen ein:\n");
     scanf("%d %d", &a, &b);
-    if (b > a) return -1;
     printf("Das GGT von %d und %d ist %d.\n", a, b, ggT(a, b));
     return 0;
 }
--- Woche6/KGV.c
+++ Woche6/KGV.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
+
 int ggT(int a, int b) {
-    if (a == 0)
-        return b;
+    if (a == 0) return b;
+
     while (b != 0) {
-        if (a > b)
+        if (a > b) {
             a = a - b;
-        else
+        } else {
             b = b - a;
+        }
     }
     return a;
 }
 
-int kgV (int a, int b) {
+int kgV(int a, int b) {
     return (a * b) / ggT(a, b);
 }
 
 int main() {
     int a, b;
-    printf("Geben Sie zuerst die groessere, dann die kleinere Zahl ein!\n");
+    printf("Geben Sie 2 Zahlen ein!\n");
     scanf("%d %d", &a, &b);
     printf("Das KGV von %d und %d ist %d.\n", a, b, kgV(a, b));
     return 0;
